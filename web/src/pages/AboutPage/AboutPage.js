5f30eec3ac5488dcfc2fab41bd5f6934479512c1
Janne Leppänen <janne@janneleppanen.com>
2020-06-07 20:48:42 +0300

    feat: Add a blog layout, an about page and a home page

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,7 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return <BlogLayout>This is a about page</BlogLayout>
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,7 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const HomePage = () => {
+  return <BlogLayout>This is a home page</BlogLayout>
+}
+
+export default HomePage
